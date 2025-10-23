fce70f253abde8e725888f84285756831ed391c3
Taner Saydam <taner_and_you@hotmail.com>
2025-10-23 09:59:10 +0300

    Add project files.

---
+++ 01CretionalPatterns/01SingletonPattern/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.WriteLine("Singleton Pattern...");
+
+Logger logger1 = Logger.Instance;
+logger1.Log("Hello world1!");
+
+Logger logger2 = Logger.Instance;
+logger2.Log("Hello world2!");
+
+Logger logger3 = Logger.Instance;
+logger3.Log("Hello world3!");
+
+Logger logger4 = Logger.Instance;
+logger4.Log("Hello world4!");
+
+Console.ReadLine();
+
+#region Static Solution
+static class StaticLogger
+{
+    public static void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+#endregion
+
+#region 1994 deki teorinin pratik çözümü
+class Logger
+{
+    private static Logger? _instance;
+    public static Logger Instance
+    {
+        get
+        {
+            if (_instance is null)
+            {
+                _instance = new Logger();
+            }
+            return _instance;
+        }
+    }
+    private Logger()
+    {
+
+    }
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+#endregion
---
+++ README.md
@@ -0,0 +1,16 @@
+﻿# Design Patterns
+## Nedir?
+**Design Patterns** yazılım geliştirmede sıkça karşılaştığımız problemler için **tekrar kullanılabilir**, **test edilmiş** ve **genel çözümler** sunan **tasarım kalıbıdır**
+
+## Kaç Çeşit Design Patterns Var?
+2 Ana grbuna ayrılmış durumda
+- Classic Design Patterns: 1994 yılında GoF tarafından yazılan Design Patterns kitabındaki patterns'lar.
+- Modenr Design Patterns: Modern teknolojilerin yaşadığı problemleri çözmek için geliştirilen modern patterns'lar
+
+## Classic Patterns Kaça Ayrılır?
+1) Creational Patterns => Nesne oluşturma problemlerine odaklanır
+2) Structural Patterns => Class -> Obje arasındaki problemlere odaklanır
+3) Behavioral Patterns => Obje -> Obje arasındaki problemlere odaklanır
+
+### 1) Creational Patterns
+- Singleton Pattern => Class'ın yalnızca bir örneğini oluşturmayı önerir
