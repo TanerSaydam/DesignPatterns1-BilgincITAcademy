d2d24077d5489f7601e8be9320ab34e6be4615b6
Taner Saydam <taner_and_you@hotmail.com>
2025-10-23 14:59:39 +0300

    Builder Pattern 2

--- 01CretionalPatterns/04BuilderPattern/Program.cs
+++ 01CretionalPatterns/04BuilderPattern/Program.cs
@@ -9,20 +9,21 @@ services.AddScoped<IHouseBuilder, HouseBuilder>();
 
 using var srv = services.BuildServiceProvider();
 #region Example 1
-IHouseBuilder houseBuilder = srv.GetRequiredService<IHouseBuilder>();
-
-House house1 = houseBuilder
+IHouseBuilder houseBuilder1 = srv.GetRequiredService<IHouseBuilder>();
+House house1 = houseBuilder1
     .WindowCount(5)
     .DoorCount(2)
     .Build();
 
-House house2 = houseBuilder
+IHouseBuilder houseBuilder2 = srv.GetRequiredService<IHouseBuilder>();
+House house2 = houseBuilder2
     .WindowCount(3)
     .DoorCount(1)
     .HaveGarden()
     .Build();
 
-House house3 = houseBuilder
+IHouseBuilder houseBuilder3 = srv.GetRequiredService<IHouseBuilder>();
+House house3 = houseBuilder3
     .WindowCount(4)
     .DoorCount(1)
     .HaveGarden()
@@ -30,6 +31,16 @@ House house3 = houseBuilder
     .Build();
 
 //House houseExample = new(windowCount: 5, doorCount: 4, haveGarden: true, havePool: false);
+#endregion
+
+#region Example 2
+MailService mailService = new();
+mailService
+    .WithFrom("tanersaydam@gmail.com")
+    .WithTo("info@saydam.com")
+    .WithSubject("Test")
+    .WithBody("Hello world")
+    .Send();
 #endregion
 
 Console.ReadLine();
@@ -64,8 +75,8 @@ class HouseBuilder : IHouseBuilder
 {
     int _windowCount;
     int _doorCount;
-    bool _haveGarden;
-    bool _havePool;
+    bool _haveGarden; //false
+    bool _havePool;//false
     public IHouseBuilder WindowCount(int count)
     {
         _windowCount = count;
@@ -96,4 +107,51 @@ class HouseBuilder : IHouseBuilder
 }
 #endregion
 
-//14:43
+#region Example 2
+class MailService
+{
+    public string From { get; set; } = default!;
+    public string To { get; set; } = default!;
+    public string Subject { get; set; } = default!;
+    public string Body { get; set; } = default!;
+    public List<string> Attachments { get; set; } = new();
+
+    public MailService WithFrom(string value)
+    {
+        From = value;
+        return this;
+    }
+
+    public MailService WithTo(string value)
+    {
+        To = value;
+        return this;
+    }
+
+    public MailService WithSubject(string value)
+    {
+        Subject = value;
+        return this;
+    }
+
+    public MailService WithBody(string value)
+    {
+        Body = value;
+        return this;
+    }
+
+    public MailService WithAttachment(List<string> values)
+    {
+        Attachments = values;
+        return this;
+    }
+    public void Send()
+    {
+        Console.WriteLine("From: {0}", From);
+        Console.WriteLine("To: {0}", To);
+        Console.WriteLine("Subject: {0}", Subject);
+        Console.WriteLine("Body: {0}", Body);
+        Console.WriteLine("Mail sended...");
+    }
+}
+#endregion
