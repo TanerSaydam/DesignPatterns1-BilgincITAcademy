37b47bab16f2d15bffd17339aea136a4a6ebf1a2
Taner Saydam <taner_and_you@hotmail.com>
2025-10-23 11:02:49 +0300

    Factory pattern 1

--- 01CretionalPatterns/01SingletonPattern/Program.cs
+++ 01CretionalPatterns/01SingletonPattern/Program.cs
@@ -1,20 +1,37 @@
-﻿Console.WriteLine("Singleton Pattern...");
+﻿using Microsoft.Extensions.DependencyInjection;
 
-Logger logger1 = Logger.Instance;
-logger1.Log("Hello world1!");
+Console.WriteLine("Singleton Pattern...");
 
-Logger logger2 = Logger.Instance;
-logger2.Log("Hello world2!");
+//Logger logger1 = Logger.Instance;
+//logger1.Log("Hello world1!");
 
-Logger logger3 = Logger.Instance;
-logger3.Log("Hello world3!");
+//Logger logger2 = Logger.Instance;
+//logger2.Log("Hello world2!");
 
-Logger logger4 = Logger.Instance;
-logger4.Log("Hello world4!");
+//Logger logger3 = Logger.Instance;
+//logger3.Log("Hello world3!");
+
+//Logger logger4 = Logger.Instance;
+//logger4.Log("Hello world4!");
+
+ServiceCollection services = new(); //DI Container
+services.AddSingleton<DILogger>();
+
+using var serviceProvider = services.BuildServiceProvider();
+DILogger diLogger1 = serviceProvider.GetRequiredService<DILogger>();
+diLogger1.Log("Hello world1!");
+
+DILogger diLogger2 = serviceProvider.GetRequiredService<DILogger>();
+diLogger2.Log("Hello world2!");
+
+DILogger diLogger3 = serviceProvider.GetRequiredService<DILogger>();
+diLogger3.Log("Hello world3!");
+
+//DILogger diLog = new();
 
 Console.ReadLine();
 
-#region Static Solution
+#region Static çözüm
 static class StaticLogger
 {
     public static void Log(string message)
@@ -28,13 +45,17 @@ static class StaticLogger
 class Logger
 {
     private static Logger? _instance;
+    private static readonly object _lock = new object();
     public static Logger Instance
     {
         get
         {
-            if (_instance is null)
+            lock (_lock) //threat safe
             {
-                _instance = new Logger();
+                if (_instance is null)
+                {
+                    _instance = new Logger();
+                }
             }
             return _instance;
         }
@@ -43,6 +64,16 @@ class Logger
     {
 
     }
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+#endregion
+
+#region Dependency Injection çözümü
+class DILogger
+{
     public void Log(string message)
     {
         Console.WriteLine(message);
---
+++ 01CretionalPatterns/02FactoryPattern/Program.cs
@@ -0,0 +1,69 @@
+﻿Console.WriteLine("Factory Pattern...");
+
+INotification notification = NotificationFactory.Create(NotificationEnum.Sms);
+notification.Send("Hello world");
+
+Console.ReadLine();
+
+interface INotification
+{
+    void Send(string message);
+}
+class EmailNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine("Sended email: {0}", message);
+    }
+}
+
+class SmsNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine("Sended sms: {0}", message);
+    }
+}
+
+class WhatsappNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine("Sended whatsapp: {0}", message);
+    }
+}
+
+class CloudeNotification : INotification
+{
+    public void Send(string message)
+    {
+        Console.WriteLine("Sended cloude: {0}", message);
+    }
+}
+
+#region 1994 deki teorinin pratik çözümü
+class NotificationFactory
+{
+    public static INotification Create(NotificationEnum type)
+    {
+        switch (type)
+        {
+            case NotificationEnum.Email: return new EmailNotification();
+            case NotificationEnum.Sms: return new SmsNotification();
+            case NotificationEnum.WP: return new WhatsappNotification();
+            case NotificationEnum.Cloude: return new CloudeNotification();
+            default:
+                throw new ArgumentException("Invalid type");
+        }
+    }
+}
+
+enum NotificationEnum
+{
+    Email,
+    Sms,
+    WP,
+    Cloude
+}
+#endregion
+
