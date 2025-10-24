9283bd304825dcfa2349ad62336980e69d7b6fab
Taner Saydam <taner_and_you@hotmail.com>
2025-10-24 14:14:12 +0300

    Proxy Pattern

---
+++ 02StructuralPatterns/07ProxyPattern/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Proxy Pattern...");
+
+ProductProxy productProxy = new();
+Product product = new("Bilgisayar", 15000);
+productProxy.Create(product);
+
+Console.ReadLine();
+
+record Product(
+    string Name,
+    decimal Price); //14:19 başlayacağız
+
+class ProductRepository
+{
+    public void Create(Product product)
+    {
+        //db ye kaydet
+        Console.WriteLine("Product create is successful!");
+    }
+}
+
+class ProductProxy
+{
+    ProductRepository productRepository = new();
+    public void Create(Product product)
+    {
+        if (product.Name.Length <= 2)
+        {
+            throw new ArgumentException("Product name must be greater than 2 words");
+        }
+        productRepository.Create(product);
+    }
+}
---
+++ 02StructuralPatterns/07ProxyPattern/README.md
@@ -0,0 +1,6 @@
+# Proxy Pattern
+Bir objeye eriþimi kontrol etmek içni onun yerine atanan vekil objesine isteðin ulaþmasý ve içinde yapýlacak kontrol/iþlemlerden sonra asýl objeye iletilmesini savunan bir pattern.
+
+Bu sayede objede asýl iþlem yapýlmadan bazý kontroller yapýlabilmesini hedefleniyor.
+
+- **Service pattern** ve **Cross-Cutting Concerns** yapýlarýna da benziyor.
