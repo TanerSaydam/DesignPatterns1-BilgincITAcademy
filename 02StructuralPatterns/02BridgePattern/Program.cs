1fca1926424998fea692a68beee13d5cb164cabe
Taner Saydam <taner_and_you@hotmail.com>
2025-10-24 09:41:43 +0300

    Bridge Pattern

---
+++ 02StructuralPatterns/02BridgePattern/Program.cs
@@ -0,0 +1,74 @@
+﻿Console.WriteLine("Bridge Pattern...");
+
+var consoleProvider = new ConsoleLogProvider();
+var fileProvider = new FileLogProvider();
+
+LogBase applog = new AppLog(consoleProvider);
+applog.Write("Hello world from app");
+
+LogBase auditlog = new AuditLog(fileProvider);
+auditlog.Write("Hello world from audit");
+
+Console.ReadLine();
+
+#region Implementation
+interface ILogProvider
+{
+    void Log(string message);
+}
+
+class ConsoleLogProvider : ILogProvider
+{
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class FileLogProvider : ILogProvider
+{
+    public void Log(string message)
+    {
+        //File.AppendText(message);
+        Console.WriteLine("File saved...");
+    }
+}
+#endregion
+
+#region Abstraction
+abstract class LogBase //primary constructor
+{
+    public readonly ILogProvider _logProvider;
+
+    protected LogBase(ILogProvider logProvider)
+    {
+        _logProvider = logProvider;
+    }
+
+    public abstract void Write(string message);
+}
+
+class AppLog : LogBase
+{
+    public AppLog(ILogProvider logProvider) : base(logProvider)
+    {
+    }
+
+    public override void Write(string message)
+    {
+        _logProvider.Log(message);
+    }
+}
+
+class AuditLog : LogBase
+{
+    public AuditLog(ILogProvider logProvider) : base(logProvider)
+    {
+    }
+
+    public override void Write(string message)
+    {
+        _logProvider.Log(message);
+    }
+}
+#endregion
---
+++ 02StructuralPatterns/02BridgePattern/README.md
@@ -0,0 +1,18 @@
+﻿# Bridge Pattern
+Soyutlama (abstraction) ile uygulamanın (implementation) birbirinden bağımsız olarak geliştirilmesini sağlayan pattern.
+
+- Ne yapıyorum => Abstraction
+- Nasıl yapıyorum => Implementation
+
+## Örnek
+Ben birden farklı yere loglama yapmak istiyorum. Loglama yaparkende tür seçmek istiyorum. 
+
+### Loglama yapacağım yer (Implementation)
+- Console Loglama
+- File Loglama
+- Db Loglama
+
+### Loglama türü (Abstraction)
+- Audit verileri
+- Error verileri
+- App verileri
